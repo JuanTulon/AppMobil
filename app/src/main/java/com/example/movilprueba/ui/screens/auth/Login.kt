d2f6f321255470cce1548970a4e6cfd537eaa295
JuanTulon <jua.verad@duocuc.cl>
2025-10-23 23:04:50 -0300

    Login casi termindo view

--- app/src/main/java/com/example/movilprueba/ui/screens/auth/Login.kt
+++ app/src/main/java/com/example/movilprueba/ui/screens/auth/Login.kt
@@ -1,69 +1,227 @@
 package com.example.movilprueba.ui.screens.auth
 
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.movilprueba.R
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import com.example.movilprueba.ui.components.TransparentTextField
+import com.example.movilprueba.ui.components.RoundedButton
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 
 class Login {
     @Composable
     fun LoginScreen() {
+
+        val emailValue = remember { mutableStateOf("") }
+        val passwordValue = remember { mutableStateOf("") }
+        var passwordVisibility by remember { mutableStateOf(false) }
+        val focusManager = LocalFocusManager.current
+
+
         Box(
             modifier =
             Modifier.fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
         ){
             Image(
+                modifier = Modifier.fillMaxSize(),
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = "Login Image",
                 contentScale = ContentScale.Inside
             )
 
-            Box(
-                modifier =  Modifier.fillMaxSize(),
-                contentAlignment = Alignment.BottomCenter
-            ){
+            ConstraintLayout(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                val (surface, fab) = createRefs()
+
                 Surface(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(400.dp),
+                        .height(400.dp)
+                        .constrainAs(surface) {
+                            bottom.linkTo(parent.bottom)
+                        },
                     color = Color.White,
                     shape = RoundedCornerShape(
                         topStart = 32.dp,
                         topEnd = 32.dp
                     )
-                ){}
-            }
+                ){
+                    Column(
+                        modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                    ){
+                        Text(
+                            text = "Bienvenido",
+                            style = MaterialTheme.typography.headlineMedium.copy(
+                                fontWeight = FontWeight.Medium
+                            )
+                        )
+                        Text(
+                            text = "Por favor inicia sesión para continuar",
+                            style = MaterialTheme.typography.headlineSmall.copy(
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        )
+                        Column(
+                            modifier = Modifier.
+                            fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ){
+                            TransparentTextField(
+                                textFieldValue = emailValue,
+                                textLabel = "Correo Electrónico",
+                                keyboardType = KeyboardType.Email,
+                                keyboardActions = KeyboardActions(
+                                    onNext = {
+                                        focusManager.moveFocus(
+                                            FocusDirection.Down)
+                                    }
+                                ),
+                                imeAction = ImeAction.Next
+                            )
 
-            FloatingActionButton(
-                modifier = Modifier.size(72.dp),
-                containerColor = MaterialTheme.colorScheme.primary,
-                onClick = {}
-            ){
-                Icon(
-                    modifier = Modifier.size(42.dp),
-                    imageVector = Icons.Default.ArrowForward,
-                    contentDescription = "Forward Icon",
-                    tint = Color.White
-                )
+                            TransparentTextField(
+                                textFieldValue = passwordValue,
+                                textLabel = "contraseña",
+                                keyboardType = KeyboardType.Password,
+                                keyboardActions = KeyboardActions(
+                                    onDone = {
+                                        focusManager.clearFocus()
+
+                                        //todo ("login")
+                                    }
+                                ),
+                                imeAction = ImeAction.Done,
+                                trailingIcon = {
+                                    IconButton(
+                                        onClick = {
+                                            passwordVisibility = !passwordVisibility
+                                        }
+                                    ){
+                                        Icon(
+                                            imageVector = if(passwordVisibility){
+                                                Icons.Filled.Visibility
+                                            }else{
+                                                Icons.Filled.VisibilityOff
+                                            },
+                                            contentDescription = "Toggle Password Icon"
+                                        )
+                                    }
+                                },
+                                visualTransformation = if(passwordVisibility){
+                                    VisualTransformation.None
+                                }else{
+                                    PasswordVisualTransformation()
+                                }
+                            )
+
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = "Olvidaste la contraseña?",
+                                style = MaterialTheme.typography.labelSmall.copy(),
+                                textAlign = TextAlign.End
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ){
+                            RoundedButton(
+                                text = "Iniciar Sesión",
+                                displayProgressBar = false,
+                                onClick = {
+                                    //todo ("login")
+                                }
+                            )
+
+                            ClickableText(
+                                text = buildAnnotatedString {
+                                    append("No tienes una cuenta? ")
+
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = MaterialTheme.colorScheme.primary,
+                                            fontWeight = FontWeight.Bold
+                                        )
+                                    ){
+                                        append("Registrarse")
+                                    }
+                                },
+                                onClick = {
+                                    //todo ("navigate to register screen")
+                                }
+                            )
+                        }
+                    }
+                }
+
+                FloatingActionButton(
+                    modifier = Modifier
+                        .size(72.dp)
+                        .constrainAs(fab) {
+                            top.linkTo(surface.top, margin = (-36).dp)
+                            end.linkTo(surface.end, margin = 36.dp)
+                        },
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    onClick = {}
+                ){
+                    Icon(
+                        modifier = Modifier.size(42.dp),
+                        imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                        contentDescription = "Forward Icon",
+                        tint = Color.White
+                    )
+                }
             }
         }
     }
